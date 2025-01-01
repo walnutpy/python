68719b807cade9d1be5c8a66ed23408363b81dc4
walnutpy <61857230+walnutpy@users.noreply.github.com>
2025-01-01 16:01:26 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 8524 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+count = [0] * 10001
+for i in range(n):
+    num = int(input())
+    count[num] += 1
+
+for i in range(10001):
+    if count[i] != 0:
+        for j in range(count[i]):
+            print(i)
