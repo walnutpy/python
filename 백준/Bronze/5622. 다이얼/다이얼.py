f87ad96a23a9cca16679acfdb20b52ecdfc336e6
walnutpy <61857230+walnutpy@users.noreply.github.com>
2025-01-20 13:40:57 +0900

    [Bronze II] Title: 다이얼, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/5622. 다이얼/다이얼.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+s = input()
+dial = ['ABC', 'DEF', 'GHI', 'JKL', 'MNO', 'PQRS', 'TUV', 'WXYZ']
+time = 0
+for i in s:
+    for j in dial:
+        if i in j:
+            time += dial.index(j)+3
+print(time)
