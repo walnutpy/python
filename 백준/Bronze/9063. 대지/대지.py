fd056d1a746bace8774bbce83bc39b98dc9e2c87
walnutpy <61857230+walnutpy@users.noreply.github.com>
2025-02-05 16:07:24 +0900

    [Bronze III] Title: 대지, Time: 1360 ms, Memory: 35916 KB -BaekjoonHub

---
+++ 백준/Bronze/9063. 대지/대지.py
@@ -0,0 +1,8 @@
+n = int(input())
+arr_x=[]
+arr_y=[]
+for _ in range(n):
+    x,y=map(int,input().split())
+    arr_x.append(x)
+    arr_y.append(y)
+print((max(arr_x)-min(arr_x)) *(max(arr_y)-min(arr_y)))
