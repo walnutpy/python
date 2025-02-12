3004ceaf9990d34cccd9c25d497172ee21ef5728
walnutpy <61857230+walnutpy@users.noreply.github.com>
2025-02-12 18:00:44 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 724 ms, Memory: 119592 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -4,36 +4,14 @@ n = int(input().rstrip())
 base_card = list(map(int,input().split()))
 m = int(input())
 having_card = list(map(int,input().split()))
-base_card.sort()
-
-def search(card):
-    left = 0
-    right = n-1
-    while True:
-        mid = (right+left)//2
-        if card < base_card[mid]:
-            right = mid - 1
-        elif card == base_card[mid]:
-            count = 0
-            for i in range(mid,right+1):
-                if base_card[i] != card:
-                    break
-                count+=1
-            for i in range(mid-1,left-1,-1):
-                if base_card[i] != card:
-                    break
-                count+=1
-            return count
-        else:
-            left = mid +1
-
-dic = {}
+dic ={}
 for i in base_card:
-    if i not in dic:
-        dic[i] = search(i)
+    if i in dic:
+        dic[i] += 1
+    else:
+        dic[i] = 1
 
 arr = []
-
 for i in having_card:
     if i in dic:
         arr.append(dic[i])
