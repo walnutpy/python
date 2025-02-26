d7d5b31224dc7a67b03cf6a8452b1a34c3bfec54
walnutpy <61857230+walnutpy@users.noreply.github.com>
2025-02-26 22:07:17 +0900

    [Silver III] Title: 01타일, Time: 256 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1904. 01타일/01타일.py
+++ 백준/Silver/1904. 01타일/01타일.py
@@ -1,12 +1,8 @@
 n = int(input())
 if n == 1:
     print(1)
-elif n == 2:
-    print(2)
 else:
-    dp = [0]*(n+1)
-    dp[1] = 1
-    dp[2] = 2
-    for i in range(3,n+1):
-        dp[i] = (dp[i-1] +dp[i-2]) % 15746
-    print(dp[n])
+    a,b=1,2
+    for _ in range(3,n+1):
+        a,b = b, (a+b)%15746
+    print(b)
